641cf31e578affdd18befeeda7df5ab33559088c
吕宏伟 <lhwgdut@gmail.com>
2026-02-02 02:15:29 +0800

    fix evaluate

--- matlab/+utils/evaluate.m
+++ matlab/+utils/evaluate.m
@@ -16,11 +16,25 @@ while hasdata(mbq)
     if ndims(logits) == 4
         logits = squeeze(logits);
     end
-    P = softmax(logits, "DataFormat","CB");
+    % If logits is a formatted dlarray, do NOT pass 'DataFormat'.
+    isFormatted = false;
+    if isa(logits,'dlarray')
+        try
+            isFormatted = ~isempty(dims(logits));
+        catch
+            isFormatted = false;
+        end
+    end
+
+    if isFormatted
+        P = softmax(logits);
+    else
+        P = softmax(logits, 'DataFormat', 'CB');
+    end
     loss = -mean(sum(T .* log(P + 1e-12), 1));
 
-    [~, pred] = max(extractdata(P), [], 1);
-    [~, gt]   = max(extractdata(T), [], 1);
+    [~, pred] = max(gather(extractdata(P)), [], 1);
+    [~, gt]   = max(gather(extractdata(T)), [], 1);
 
     numCorrect = numCorrect + sum(pred == gt);
     numTotal   = numTotal + numel(gt);
