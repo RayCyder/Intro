526286758216c3dbd787b46d6db5ff72ba379987
吕宏伟 <lhwgdut@gmail.com>
2026-02-02 00:12:40 +0800

    download data

--- matlab/+data/makeCIFAR10Datastores.m
+++ matlab/+data/makeCIFAR10Datastores.m
@@ -1,4 +1,12 @@
 function [dsTrain, dsTest, classNames] = makeCIFAR10Datastores(rootDir)
+% Download CIFAR-10 (matlab version) to current folder if missing
+url = "https://www.cs.toronto.edu/~kriz/cifar-10-matlab.tar.gz";
+out = fullfile(pwd, "cifar-10-matlab.tar.gz");
+if ~isfile(out)
+    websave(out, url);
+end
+untar(out, pwd);   % extracts cifar-10-batches-mat
+
 trainDir = fullfile(rootDir, "train");
 testDir  = fullfile(rootDir, "test");
 
